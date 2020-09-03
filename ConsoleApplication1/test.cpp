866f8fcbd9ff2b4c2f0e26a169921d05feacd64b
JesusPaquette <66388884+JesusPaquette@users.noreply.github.com>
2020-09-03 16:45:23 -0400

    Update test.cpp
    pour gerer efficacement la convertion des caractere accentuer

--- ConsoleApplication1/test.cpp
+++ ConsoleApplication1/test.cpp
@@ -10,6 +10,10 @@
 int main(/*liste des parametre eventuels*/)
 { // un bloc d<instruction
 	
+	// pour gerer efficacement la convertion des caractere accentuer 
+	setlocal(LC
+	
+	
 	// Afficher le message a l'ecran
 	std::cout << "suce pute";
 
