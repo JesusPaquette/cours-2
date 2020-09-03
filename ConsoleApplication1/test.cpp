3474655bfac264213bf679091669b0d605f06d68
JesusPaquette JesusPaquette <nicolaspaquette665@gmail.com>
2020-09-03 16:20:16 -0400

    programme qui affiche in message a l'ecran teste le probleme des accents

--- ConsoleApplication1/test.cpp
+++ ConsoleApplication1/test.cpp
@@ -4,13 +4,14 @@
 
 
 
-
+#include<iostream>      //integre un coffre a outils pour communiquer avec l'utilisateur
 
 
 int main(/*liste des parametre eventuels*/)
 { // un bloc d<instruction
 	
-
+	// Afficher le message a l'ecran
+	std::cout << "suce pute";
 
 
 
